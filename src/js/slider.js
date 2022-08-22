ba9212422f7af5207677f555ea9f8c6a330ddf2f
Ann Serdukova <404region@gmail.com>
2022-08-22 12:55:12 +0300

    Верстаю дальше

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,12 +7,14 @@ window.addEventListener('resize', function(event) {
     if(window.innerWidth <= '768') {
         addSwiper();
         addSwiperRepairTypes();
+        addSwiperPrices();
     }
 }, true);
 
 if(window.innerWidth <= '768') {
     addSwiper();
     addSwiperRepairTypes();
+    addSwiperPrices();
 }
 
 function addSwiper() {
@@ -41,6 +43,31 @@ function addSwiper() {
 
 function addSwiperRepairTypes() {
     const slider =  document.querySelector('.swiper-container-2');
+    console.log('slider', slider);
+
+    const swiper = new Swiper(slider, {
+        slidesPerView: 'auto',
+        spaceBetween: 32,
+        grabCursor: true,
+        mousewheel: {
+            sensitivity: 1,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        }
+    });
+}
+
+//prices
+function addSwiperPrices() {
+    const slider =  document.querySelector('.swiper-container-prices');
     console.log('slider', slider);
 
     const swiper = new Swiper(slider, {
