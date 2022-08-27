3789236d92b006724c0f20f778adb05e42e1ff25
Ann Serdukova <404region@gmail.com>
2022-08-27 03:51:25 +0300

    Причесываю стили

--- src/js/submit.js
+++ src/js/submit.js
@@ -7,12 +7,14 @@
     сохранить состояние
 */
 let submitWindowOpenState = false; // окно Оствить заявку закрыто по умолчанию
+let checkRepairWindowOpenState = false; // окно Оствить заявку закрыто по умолчанию
 
 const submitBtn = document.querySelector('.submit-btn');
 const submitBtnsBlock = document.querySelector('.submit-block-btns');
 
 const modalWindowSudmit = document.querySelector('.modal-window--submit');
-const modalWindow = document.querySelector('.modal-window');
+const modalWindowCheckRepair = document.querySelector('.modal-window--check-repair');
+const modalWindows =  Array.from(document.getElementsByClassName('modal-window'));
 
 const modalBackground = document.querySelector('.modal');
 
@@ -20,7 +22,11 @@ submitBtnsBlock.addEventListener('click', (event) => {
     if(event?.target?.classList.contains('submit-btn')) {
         //Отобразить окно "Заполнить заявку"
         //Отобразить модальное окно
-
+        
+        modalWindowSudmit.classList.add('modal-window--active');
+        modalWindowSudmit.classList.remove('modal-window--inactive');
+        modalBackground.classList.add('modal-active');
+        /*
         if(!submitWindowOpenState ) {
             modalWindowSudmit.classList.add('modal-window--active');
             modalWindowSudmit.classList.remove('modal-window--inactive');
@@ -32,19 +38,41 @@ submitBtnsBlock.addEventListener('click', (event) => {
         }
 
         submitWindowOpenState = !submitWindowOpenState;
+        */
     }
 
     if(event?.target?.classList.contains('check-repair-status-btn')) {
         //Отобразить окно "Проверить статус"
         //Отобразить модальное окно
+
+       
+        modalWindowCheckRepair.classList.add('modal-window--active');
+        modalWindowCheckRepair.classList.remove('modal-window--inactive');
+        modalBackground.classList.add('modal-active');
+
+        /*
+        if(!checkRepairWindowOpenState ) {
+            modalWindowCheckRepair.classList.add('modal-window--active');
+            modalWindowCheckRepair.classList.remove('modal-window--inactive');
+            modalBackground.classList.add('modal-active');
+        } else {
+            modalWindowCheckRepair.classList.add('modal-window--inactive');
+            modalWindowCheckRepair.classList.remove('modal-window--active');
+            modalBackground.classList.remove('modal-active');
+        }
+
+        checkRepairWindowOpenState = !checkRepairWindowOpenState;
+        */
        
     }
 });
 
-modalWindow.addEventListener('click', (event) => {
-    if(event?.target?.classList.contains('model-window__close-btn')) {
-        modalWindow.classList.remove('modal-window--active');
-        modalWindow.classList.add('modal-window--inactive');
-        modalBackground.classList.remove('modal-active');
-    }
+modalWindows.map((modalWindow) => {
+    modalWindow.addEventListener('click', (event) => {
+        if(event?.target?.classList.contains('model-window__close-btn')) {
+            modalWindow.classList.remove('modal-window--active');
+            modalWindow.classList.add('modal-window--inactive');
+            modalBackground.classList.remove('modal-active');
+        }
+    });
 });
